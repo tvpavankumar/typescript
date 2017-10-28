163332d665632c80b5aa0e140a83d8572497906d
Tummala VenkataPavanKumar <tvenkatapavankumar@yahoo.co.in>
2017-10-28 11:50:47 -0500

    added a new typrscript solution

---
+++ datatypes.ts
@@ -0,0 +1,12 @@
+let fname : string= "Pavan"
+var fstname = "Venkata"
+
+let isValid:boolean;
+console.log(isValid)
+
+let dob:Date = new Date('11-nov-2013');
+console.log(dob);
+
+let uniontypes : string | number;
+uniontypes= 10;
+uniontypes = "Pavan";
